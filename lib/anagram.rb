a2f44e3d837adf30303049a13bfeca141f874dc3
Ryan Parrish <rino3717@gmail.com>
2020-04-04 17:58:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,14 @@
 class Anagram
-  
+
   attr_accessor :word
 
-  def intialize(word)
+  def initialize(word)
     @word = word
   end
-  
-  def match(words_arr)
-    words_arr.select do |word|
-      words.split("").sort == @word.split("").sort
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
     end
   end
 end
