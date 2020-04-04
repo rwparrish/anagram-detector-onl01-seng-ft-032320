e5acff1b2334e77a43034085ad64726132cac899
Ryan Parrish <rino3717@gmail.com>
2020-04-04 17:59:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,8 @@ class Anagram
     @word = word
   end
 
-  def match(word_array)
-    word_array.select do |word|
+  def match(words_arr)
+    words_arr.select do |word|
       word.split("").sort == @word.split("").sort
     end
   end
